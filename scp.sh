47c184b1f658196487d82d721a98f7ed675e68de
basdevelop <hyperorchid@outlook.com>
2024-11-21 10:23:03 +0800

    add db tool logic

--- scp.sh
+++ scp.sh
@@ -1,2 +1,2 @@
-scp -i ~/Documents/conf/dessage-rbc ./bin/*.lnx ribencong@35.220.156.3:~/bmail/
+scp -i ~/Documents/conf/dessage-rbc ./bin/*.lnx ribencong@35.247.142.146:~/
 scp ./bin/*.lnx root@216.128.182.150:/root/bmail/
